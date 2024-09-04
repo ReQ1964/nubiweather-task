b7ff7a987d01962bd79b13dad152d9af4d600fa1
ReQ1964 <slomianykacper4@gmail.com>
2024-09-04 09:57:33 +0200

    feat: :sparkles: add Swiper to DayForecast

--- apps/frontend/src/components/Forecast/DayForecast.tsx
+++ apps/frontend/src/components/Forecast/DayForecast.tsx
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
+import dayjs from 'dayjs';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import { useForecastData } from '@/hooks/useForecastData';
 import ForecastTile from './ForecastTile';
-import dayjs from 'dayjs';
-import { useContext } from 'react';
 import { CurrentCityContext } from '@/App';
-import { Fragment } from 'react';
+
+import 'swiper/css';
 
 type ForecastHour = {
   condition: {
@@ -26,22 +29,7 @@ const filterHours = (
       // return today's forecast only ahead of current time, not for the whole day
     })
     .slice(0, dayIndex === 1 ? 7 : undefined);
-  // return tommorow's forecast only for first 7 hours
-};
-
-const renderForecastTiles = (hours: ForecastHour[]) => {
-  return hours.map(({ time, temp_c, condition }) => {
-    const hour = dayjs(time).format('HH:00');
-    return (
-      <ForecastTile
-        key={time}
-        topInfo={hour}
-        temperature={temp_c}
-        weatherIcon={condition.icon}
-        weatherText={condition.text}
-      />
-    );
-  });
+  // return tomorrow's forecast only for the first 7 hours
 };
 
 const DayForecast = () => {
@@ -59,12 +47,47 @@ const DayForecast = () => {
   const currentHour = dayjs().format('HH:00');
 
   return (
-    <main className='flex flex-row gap-7'>
-      {forecastDays.map((day, index) => {
-        const hours = filterHours(day.hour, index, currentHour);
-        return <Fragment key={day.date}>{renderForecastTiles(hours)}</Fragment>;
-      })}
-    </main>
+    <div className='flex flex-row gap-7'>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={2}
+        breakpoints={{
+          375: {
+            slidesPerView: 2,
+          },
+          450: {
+            slidesPerView: 3,
+          },
+          550: {
+            slidesPerView: 4,
+          },
+          700: {
+            slidesPerView: 5,
+          },
+          900: {
+            slidesPerView: 6,
+          },
+        }}
+      >
+        {forecastDays.map((day, index) => {
+          const hours = filterHours(day.hour, index, currentHour);
+
+          return hours.map(({ time, temp_c, condition }) => {
+            const hour = dayjs(time).format('HH:00');
+            return (
+              <SwiperSlide key={time}>
+                <ForecastTile
+                  topInfo={hour}
+                  temperature={temp_c}
+                  weatherIcon={condition.icon}
+                  weatherText={condition.text}
+                />
+              </SwiperSlide>
+            );
+          });
+        })}
+      </Swiper>
+    </div>
   );
 };
 
