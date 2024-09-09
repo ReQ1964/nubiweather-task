e32fe2ffc066b59302445917a9cbea9edf46e086
ReQ1964 <slomianykacper4@gmail.com>
2024-09-09 10:52:56 +0200

    feat: :sparkles: add zod error handling to hooks

--- apps/frontend/src/hooks/useForecastData.ts
+++ apps/frontend/src/hooks/useForecastData.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { API_KEY, API_URL } from '../constants/api';
 import { CityName, ForecastData } from 'shared-types/apiTypes';
 import { FetchForecastResult } from 'shared-schemas/apiSchemas';
+import { ZodError } from 'zod';
 
 const fetchForecastData = async (city: CityName, days: number) => {
   const res = await axios.get(`${API_URL}forecast.json`, {
@@ -14,9 +15,21 @@ const fetchForecastData = async (city: CityName, days: number) => {
       alerts: 'no',
     },
   });
+  try {
+    const parsedData = FetchForecastResult.parse(res.data);
+    return parsedData;
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const path = error.issues[0].path.join(' -> ');
+      const formattedError = `Error: ${error.issues[0].message} | [${path}]`;
 
-  const parsedData = FetchForecastResult.parse(res.data);
-  return parsedData;
+      console.error('Zod validation error:', formattedError);
+      throw new Error(formattedError);
+    } else {
+      console.error('Unexpected error:', error);
+      throw new Error('An unexpected error occurred');
+    }
+  }
 };
 
 export const useForecastData = (currentCity: CityName, days: number) => {
