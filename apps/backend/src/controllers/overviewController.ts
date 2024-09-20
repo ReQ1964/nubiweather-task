6705bc396d50d89b0643e5d6fe9cac12902bd366
ReQ1964 <slomianykacper4@gmail.com>
2024-09-20 13:52:18 +0200

    feat: :sparkles: add basic prisma setup, add axios and fetch freeweatherapi data

--- apps/backend/src/controllers/overviewController.ts
+++ apps/backend/src/controllers/overviewController.ts
@@ -1,15 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+import axios from 'axios';
 
 export const checkForQueryErrors = (
   req: Request,
   _res: Response,
   next: NextFunction
 ) => {
-  const { cityParam } = req.query;
-  !cityParam ? next(new Error('City query parameter is required!')) : next();
+  const { city } = req.query;
+  !city ? next(new Error('City query parameter is required!')) : next();
 };
 
-export const getTodayOverview = (req: Request, res: Response) => {
-  const { cityParam } = req.query;
-  res.json(cityParam);
+export const getTodayOverview = async (req: Request, res: Response) => {
+  const { city } = req.query;
+  const { data } = await axios.get(`${process.env.API_URL}/current.json`, {
+    params: {
+      key: process.env.API_KEY,
+      q: city,
+      aqi: 'no',
+    },
+  });
+
+  console.log(data);
+  res.json(data);
 };
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,19 @@
 import app from './app';
+import { prisma } from './prismaClient';
+
+async function main() {
+  const weatherData = await prisma.weatherData.findMany();
+  console.log(weatherData);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
