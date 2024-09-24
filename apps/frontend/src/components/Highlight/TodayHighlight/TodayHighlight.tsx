de0b1f2b878ae1fad2d1b5557e666d6b75d844c3
ReQ1964 <slomianykacper4@gmail.com>
2024-09-24 12:59:51 +0200

    feat: :sparkles: introduce useDataFetching hook to TodayHighlight and WeatherOverview

--- apps/frontend/src/components/Highlight/TodayHighlight/TodayHighlight.tsx
+++ apps/frontend/src/components/Highlight/TodayHighlight/TodayHighlight.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import UVIndexIcon from '@/assets/icons/weatherStatus/UVIndexIcon';
 import HumidityIcon from '@/assets/icons/weatherStatus/HumidityIcon';
 import HeatIndexIcon from '@/assets/icons/weatherStatus/HeatIndexIcon';
@@ -5,67 +6,67 @@ import VisibilityIcon from '@/assets/icons/weatherStatus/VisibilityIcon';
 import WindIcon from '@/assets/icons/weatherStatus/WindIcon';
 import RainPrecipitationIcon from '@/assets/icons/weatherStatus/RainPrecipitationIcon';
 import { TodayHighlightTile } from '../TodayHighlightTile/TodayHighlightTile';
+import { useApiData } from '@/hooks/useApiData/useApiData';
+import { CurrentCityContext } from '@/App';
+import { TodayHighlightSchema } from 'shared-schemas/apiSchemas';
+import { TodayHighlightSchemaType } from 'shared-types/apiTypes';
+import { TodayHighlightSkeleton } from '../TodayHighlightTile/TodayHighlightTileSkeleton';
+import { useDataFetching } from '@/hooks/useDataFetching/useDataFetching';
 
-interface TodayHighlightProps {
-  humidity: number;
-  heatIndex: number;
-  uv: number;
-  precipitation: number;
-  windSpeed: number;
-  visibility: number;
-}
+const TodayHighlight = () => {
+  const { currentCity } = useContext(CurrentCityContext);
 
-export interface TodayHighlightTileProps {
-  text: string;
-  heading: string;
-  icon?: JSX.Element; // Prop for the dynamic icon component
-}
-
-const TodayHighlight = ({
-  humidity,
-  heatIndex,
-  uv,
-  precipitation,
-  windSpeed,
-  visibility,
-}: TodayHighlightProps) => {
-  return (
-    <main>
-      <h2 className='mb-6 text-lg font-bold'>Today&apos;s Highlights</h2>
-      <section className='grid grid-cols-2 gap-7 md:grid-cols-3'>
-        <TodayHighlightTile
-          heading='UV Index'
-          text={uv.toString()}
-          icon={<UVIndexIcon />}
-        />
-        <TodayHighlightTile
-          heading='Wind Speed'
-          text={`${windSpeed} km/h`}
-          icon={<WindIcon />}
-        />
-        <TodayHighlightTile
-          heading='Visibility'
-          text={`${visibility} km`}
-          icon={<VisibilityIcon />}
-        />
-        <TodayHighlightTile
-          heading='Humidity'
-          text={`${humidity}%`}
-          icon={<HumidityIcon />}
-        />
-        <TodayHighlightTile
-          heading='Rain Chance'
-          text={`${precipitation} mm`}
-          icon={<RainPrecipitationIcon />}
-        />
-        <TodayHighlightTile
-          heading='Heat Index'
-          text={`${heatIndex}°C`}
-          icon={<HeatIndexIcon />}
-        />
-      </section>
-    </main>
+  const fetchResult = useApiData<TodayHighlightSchemaType>(
+    currentCity,
+    'http://localhost:5000/todayHighlight',
+    TodayHighlightSchema,
   );
+
+  return useDataFetching({
+    fetchResult,
+    loadingComponent: <TodayHighlightSkeleton />,
+    errorClassName: 'h-full min-h-[370px]',
+    renderData: (data) => {
+      const { humidity, uv, wind_kph, precip_mm, vis_km, heatindex_c } = data;
+      return (
+        <main>
+          <h2 className="mb-6 text-lg font-bold">Today&apos;s Highlights</h2>
+          <section className="grid grid-cols-2 gap-7 md:grid-cols-3">
+            <TodayHighlightTile
+              heading="UV Index"
+              text={uv}
+              icon={<UVIndexIcon />}
+            />
+            <TodayHighlightTile
+              heading="Wind Speed"
+              text={`${wind_kph} km/h`}
+              icon={<WindIcon />}
+            />
+            <TodayHighlightTile
+              heading="Visibility"
+              text={`${vis_km} km`}
+              icon={<VisibilityIcon />}
+            />
+            <TodayHighlightTile
+              heading="Humidity"
+              text={`${humidity}%`}
+              icon={<HumidityIcon />}
+            />
+            <TodayHighlightTile
+              heading="Rain Chance"
+              text={`${precip_mm} mm`}
+              icon={<RainPrecipitationIcon />}
+            />
+            <TodayHighlightTile
+              heading="Heat Index"
+              text={`${heatindex_c}°C`}
+              icon={<HeatIndexIcon />}
+            />
+          </section>
+        </main>
+      );
+    },
+  });
 };
 
 export default TodayHighlight;
--- apps/frontend/src/components/Overview/WeatherOverview/WeatherOverview.tsx
+++ apps/frontend/src/components/Overview/WeatherOverview/WeatherOverview.tsx
@@ -1,75 +1,59 @@
 import dayjs from 'dayjs';
 import cityImg from '@/assets/img/city-bg.jpg';
 import CityAutocompleteInput from '../CityAutocompleteInput/CityAutocompleteInput';
-import { useContext } from 'react';
+import { ReactNode, useContext } from 'react';
 import { CurrentCityContext } from '@/App';
-import LoadingSpinner from '@/assets/icons/LoadingSpinner';
 import { CurrentWeatherSchema } from 'shared-schemas/apiSchemas';
 import { CurrentWeatherSchemaType } from 'shared-types/apiTypes';
 import { useApiData } from '@/hooks/useApiData/useApiData';
+import { useDataFetching } from '@/hooks/useDataFetching/useDataFetching';
 
-const WeatherOverview = (): JSX.Element => {
+const WeatherOverview = (): ReactNode => {
   const { currentCity } = useContext(CurrentCityContext);
-  const { data, error, isLoading, isError } =
-    useApiData<CurrentWeatherSchemaType>(
-      currentCity,
-      'http://localhost:5000/currentWeather',
-      CurrentWeatherSchema,
-    );
-
-  if (isLoading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <LoadingSpinner />
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <div>{error?.message || 'An unexpected error occurred'}</div>;
-  }
-
-  if (!data) {
-    return <div>No data available</div>;
-  }
-
-  const {
-    name,
-    country,
-    localtime,
-    temp_c,
-    condition,
-    icon,
-  }: CurrentWeatherSchemaType = data;
-  const formattedDate = dayjs(localtime).format('dddd, D MMMM');
-
-  return (
-    <section className="flex flex-col px-4 py-6 lg:w-3/12 lg:rounded-l-3xl lg:p-8">
-      <header className="flex flex-col gap-4">
-        <nav className="relative flex flex-col gap-4">
-          <div>
-            <h2 className="mr-7 text-3xl">
-              {name}, {country}
-            </h2>
-            <p>{formattedDate}</p>
-          </div>
-          <CityAutocompleteInput />
-        </nav>
-      </header>
-      <main className="flex flex-row justify-center pb-6 pt-10 sm:py-12 lg:flex-col lg:py-0 lg:pt-6 lg:text-center">
-        <img src={icon} alt={condition} className="w-28 lg:self-center" />
-        <div className="flex flex-col justify-center gap-2">
-          <h1 className="text-5xl">{temp_c}&#8451;</h1>
-          <p>{condition}</p>
-        </div>
-      </main>
-      <img
-        src={cityImg}
-        alt="City view"
-        className="mt-auto hidden rounded-2xl brightness-90 lg:block"
-      />
-    </section>
+  const fetchResult = useApiData<CurrentWeatherSchemaType>(
+    currentCity,
+    'http://localhost:5000/currentWeather',
+    CurrentWeatherSchema,
   );
+
+  return useDataFetching({
+    fetchResult,
+    errorClassName: 'px-4 py-6 lg:w-3/12 lg:rounded-l-3xl lg:p-8',
+    loadingClassName:
+      'flex items-center justify-center px-4 py-20 lg:w-3/12 lg:rounded-l-3xl lg:p-8',
+    renderData: (data) => {
+      const { name, country, localtime, temp_c, condition, icon } = data;
+      const formattedDate = dayjs(localtime).format('dddd, D MMMM');
+
+      return (
+        <section className="flex flex-col px-4 py-6 lg:w-3/12 lg:rounded-l-3xl lg:p-8">
+          <header className="flex flex-col gap-4">
+            <nav className="relative flex flex-col gap-4">
+              <div>
+                <h2 className="mr-7 text-3xl">
+                  {name}, {country}
+                </h2>
+                <p>{formattedDate}</p>
+              </div>
+              <CityAutocompleteInput />
+            </nav>
+          </header>
+          <main className="flex flex-row justify-center pb-6 pt-10 sm:py-12 lg:flex-col lg:py-0 lg:pt-6 lg:text-center">
+            <img src={icon} alt={condition} className="w-28 lg:self-center" />
+            <div className="flex flex-col justify-center gap-2">
+              <h1 className="text-5xl">{temp_c}&#8451;</h1>
+              <p>{condition}</p>
+            </div>
+          </main>
+          <img
+            src={cityImg}
+            alt="City view"
+            className="mt-auto hidden rounded-2xl brightness-90 lg:block"
+          />
+        </section>
+      );
+    },
+  });
 };
 
 export default WeatherOverview;
